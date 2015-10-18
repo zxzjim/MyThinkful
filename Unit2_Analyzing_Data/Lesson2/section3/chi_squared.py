e64d3df353721788658c8d8f3106df0ce7bc86f3
Xinzhou Zhang <zxzjim426@gmail.com>
2015-10-19 00:31:32 +0200

    bar.png added

--- Unit2_Analyzing_Data/Lesson2/section3/chi_squared.py
+++ Unit2_Analyzing_Data/Lesson2/section3/chi_squared.py
@@ -9,7 +9,7 @@ loansData.dropna(inplace=True)
 freq = collections.Counter(loansData['Open.CREDIT.Lines'])
 plt.figure()
 plt.bar(freq.keys(), freq.values(), width = 1)
-plt.show()
+plt.savefig('bar.png')
 
 chi, p = stats.chisquare(freq.values())
 print chi, p
