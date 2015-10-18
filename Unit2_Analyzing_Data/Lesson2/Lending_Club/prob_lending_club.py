03b84e7d50f6bffc7e355e40903325dd16ae551e
Xinzhou Zhang <zxzjim426@gmail.com>
2015-10-18 23:42:31 +0200

    lending club challenge commit

---
+++ Unit2_Analyzing_Data/Lesson2/Lending_Club/prob_lending_club.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+
+loansData = pd.read_csv('loansData.csv')
+loansData.dropna(inplace = True)
+
+loansData.boxplot(column='Amount.Requested')
+plt.savefig('boxplot.png')
+
+loansData.hist(column = 'Amount.Requested')
+plt.savefig('hist.png')
+
+plt.figure()
+graph = stats.probplot(loansData['Amount.Requested'], dist = 'norm', plot = plt)
+plt.savefig('qqplot.png')
